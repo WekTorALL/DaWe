e7ae00ea243b1a77a576c85f4f442412011af71d
WekTorALL <wektorall@gmail.com>
2015-05-19 23:15:46 +0300

    Table data with rowid

--- content/table_data.php
+++ content/table_data.php
@@ -11,17 +11,21 @@ while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
 }
 echo "<td><b>Action</b></td>";
 echo "</tr>\n";
-$query='SELECT * FROM '.$_GET['tableName'];
+$query='SELECT rowidtochar(rowid) r, a.* FROM '.$_GET['tableName'].' a';
 echo $query;
 $stid = oci_parse($conn, $query);
 oci_execute($stid);
 while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
     echo "<tr>\n";
+    $row_id=false;
     foreach ($row as $item) {
-        echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+        if($row_id==true){
+            echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+        }
+        $row_id=true;
     }
-    $get_row_id='select rowid from '.$_GET['tableName'].' where ';
-    echo "<td><a href='edit_row.php?row='.$row>Edit</a></td>";
+    echo "<td><a href='edit_row.php?row=".$row['R']."&tableName=".$_GET['tableName']."'>Edit</a></td>";
+
     echo "</tr>\n";
 }
 echo "</table>\n";
